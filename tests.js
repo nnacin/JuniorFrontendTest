139feff3af45306844f671c57fe338cc1c7954ef
Natali Nacinovic <nnacinovic@dickeys.com>
2017-05-07 16:11:21 +0200

    added some test (not functional)

---
+++ tests.js
@@ -0,0 +1,32 @@
+QUnit.test( "hello test", function( assert ) {
+    assert.ok( 1 == "1", "Passed!" );
+});
+
+QUnit.test("test empty username field", function(assert) {
+    var result = gitHubSearch();
+    assert.ok(result == false, "Passed!" );
+});
+
+QUnit.test("test searched username exist #1", function(assert) {
+    document.getElementById("username").value = "octocat";
+    var result = gitHubSearch();
+    assert.ok( result == true, "Passed!" );
+});
+
+QUnit.test("test searched username exist #2", function(assert) {
+    document.getElementById("username").value = "nnacin";
+    var result = gitHubSearch();
+    assert.ok( result == true, "Passed!" );
+});
+
+QUnit.test("test searched username does not exist", function(assert) {
+    document.getElementById("username").value = "nnacinoo";
+    var result = gitHubSearch();
+    assert.ok( result == null, "Passed!" );
+});
+
+QUnit.test("test searched username exist #3", function(assert) {
+    document.getElementById("username").value = "octodog";
+    var result = gitHubSearch();
+    assert.ok( result == true, "Passed!" );
+});
